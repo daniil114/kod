14ff715968e63d01ef83552bd20ece0a4d5d2b4e
daniil114 <danalexeev2011@gmail.com>
2024-11-14 19:57:29 +0300

    site.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-print ("BYE DANYA BEKOUSE IM BEST OF THE  BEST")
-print ("BYE DANYA BEKOUSE IM BEST OF THE  BEST")
-print ("BYE DANYA BEKOUSE IM BEST OF THE  BEST")
-print ("BYE DANYA BEKOUSE IM BEST OF THE  BEST")
+import random
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def random_dice():
+    return str(random.randint(1, 6))
+
+@app.route("/")
+def index():
+    return render_template("index.html", dice=random_dice())
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5003)

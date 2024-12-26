f7e873af698ceee0fbb14e08b525ff52a22622f6
daniil114 <danalexeev2011@gmail.com>
2024-12-26 19:59:54 +0300

    Update models.py

--- models.py
+++ models.py
@@ -12,6 +12,7 @@ class User(db.Model):
     city = db.Column(db.String(50), nullable=False)
     education = db.relationship('Education', backref='user', lazy='dynamic')
     contact = db.relationship("Contact", backref="user", lazy="dynamic")
+    project = db.relationship("Project", backref="user", lazy="dynamic")
 
     def __repr__(self):
         return f'<User {self.name}>'
@@ -30,3 +31,13 @@ class Contact(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     phone = db.Column(db.String(15), nullable=False)
     tg = db.Column(db.String(25), nullable=False)
+
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    link = db.Column(db.String(200), nullable=False, default="/")
+    pic = db.Column(db.String(100), nullable=False)
+    filename = (db.Text, unique=True, nullable=False)
